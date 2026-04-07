706d9ccb9fbca03a45afe1eee7afa49b9690b834
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2026-04-07 12:06:00 +0700

    Task 2 - The Camera Eye: Vision Setup & Permission
    Update pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:logbook_app_080/features/logbook/models/log_model.dart';
 import 'package:logbook_app_080/features/onboarding/onboarding_view.dart';
+import 'package:logbook_app_080/helpers/log_helper.dart';
+
+List<CameraDescription> cameras = [];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
+
+  try {
+    cameras = await availableCameras();
+    await LogHelper.writeLog(
+      'Available cameras: ${cameras.length}',
+      source: 'main.dart',
+      level: 2,
+    );
+  } on CameraException catch (e) {
+    await LogHelper.writeLog(
+      'Camera Error: ${e.code}\nError Message: ${e.description}',
+      source: 'main.dart',
+      level: 1,
+    );
+  }
 
   await Hive.initFlutter();
   Hive.registerAdapter(LogModelAdapter());
